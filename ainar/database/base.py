727830ca7367c362791df16721e4069ef21e8356
odoku <m.onogawa@gmail.com>
2022-01-05 16:08:48 +0900

    Added model classes and data definitions for coco.

---
+++ ainar/database/base.py
@@ -0,0 +1,7 @@
+import peewee as pw
+
+proxy = pw.Proxy()
+
+class BaseModel(pw.Model):
+  class Meta:
+    database = proxy
---
+++ ainar/database/coco.py
@@ -0,0 +1,28 @@
+import peewee as pw
+from playhouse.sqlite_ext import JSONField
+
+from ainar.database.base import BaseModel
+
+
+class Image(BaseModel):
+    width = pw.FloatField()
+    height = pw.FloatField()
+    file_name = pw.CharField()
+    license = pw.IntegerField()
+    flickr_url = pw.CharField()
+    coco_url = pw.CharField()
+    date_captured = pw.DateTimeField()
+
+
+class Category(BaseModel):
+    name = pw.CharField()
+    supercategory = pw.CharField()
+
+
+class Annotation(BaseModel):
+    image = pw.ForeignKeyField(Image, backref="annotations")
+    category = pw.ForeignKeyField(Category, backref="annotations")
+    bbox = JSONField()
+    segmentation = JSONField()
+    area = pw.FloatField()
+    iscrowd = pw.BooleanField()
