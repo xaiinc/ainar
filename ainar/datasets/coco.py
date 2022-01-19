00e181add3cf97f66d853051479d3bb1cc14d2d7
masato0412 <masato41280@icloud.com>
2022-01-19 15:17:58 +0900

    coco dataset

--- ainar/datasets/coco.py
+++ ainar/datasets/coco.py
@@ -1,52 +1,24 @@
-from pathlib import Path
-from typing import List, Tuple, TypedDict, Union
+from typing import Any, Dict, List
+
+from pycocotools.coco import COCO
 
 
-class Image(TypedDict):
-    id: int
-    width: int
-    height: int
-    file_name: str
-    license: int
-    flickr_url: str
-    coco_url: str
-    date_captured: str
+class CocoDetection:
+    def __init__(
+        self,
+        annotation_file: str,
+    ) -> None:
 
+        self.coco = COCO(annotation_file)
+        self.ids = list(sorted(self.coco.imgs.keys()))
 
-class Category(TypedDict):
-    id: int
-    name: str
-    supercategory: str
+    def _load_annotation(self, id: int) -> List[Any]:
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
 
+    def __getitem__(self, index: int) -> List[Dict]:
+        id = self.ids[index]
+        annotation = self._load_annotation(id)
+        return annotation
 
-class Segmentation(TypedDict):
-    counts: List[int]
-    size: Tuple[int, int]
-
-
-class Annotation(TypedDict):
-    id: int
-    image_id: int
-    category_id: int
-    bbox: Tuple[int, int, int, int]  # [x, y, width, height]
-    segmentation: Union[List[List[float]], Segmentation]
-    area: float
-    iscrowd: int  # 0 or 1
-
-
-class Coco:
-    @property
-    def categories(self) -> List[Category]:
-        return []
-
-    @property
-    def images(self) -> List[Image]:
-        return []
-
-    @property
-    def annotations(self) -> List[Annotation]:
-        return []
-
-
-def load_dataset(path: Path):
-    return Coco()
+    def __len__(self) -> int:
+        return len(self.ids)
