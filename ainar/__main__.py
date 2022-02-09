26cf67e3106620e03b94981066a33bf05a82dd35
おどく <odoku@users.noreply.github.com>
2022-02-09 17:00:28 +0900

    フィルター出来る項目を追加。 (#23)
    * フィルター出来る項目を追加。
    
    * 演算子の優先順序を考慮してなかったのを修正。
    
    * Fixed lint error.

--- ainar/__main__.py
+++ ainar/__main__.py
@@ -8,11 +8,11 @@ import click
 from ainar.database import proxy
 from ainar.database.coco import Annotation, Category, Image
 from ainar.datasets.coco import Coco, load_dataset
-from ainar.filters import filter_annotations, parse_filter_string
+from ainar.filters import filter_dataset, parse_filter_string
 
 
 @click.command()
-@click.argument("data_dir", type=Path)
+@click.argument("dataset_json", type=click.Path(exists=True))
 @click.option("--cache", type=Path, default=None, help="")
 @click.option("--filter", default=None, help="")
 @click.option("--output", default=None, help="")
@@ -30,10 +30,10 @@ def main(**args):
     # TODO: Add force analize option
     # Analize dataset if cache does not exists
     if not has_cache:
-        store_dataset(db, load_dataset(args["data_dir"]))
+        store_dataset(db, load_dataset(args["dataset_json"]))
 
     # Filtering
-    annotations = filter_annotations(Annotation.select(), args["filter"])
+    [categories, images, annotations] = filter_dataset(args["filter"])
 
     # Aggregation
     data = aggregate(annotations, args["filter"])
@@ -43,40 +43,50 @@ def main(**args):
 
     # Export to file
     if args["export"]:
-        export(data)
+        export(categories, images, annotations)
 
 
 def validate_args(
-    data_dir: Path,
+    dataset_json: Path,
     cache: Optional[str],
     filter: Optional[str],
     output: Optional[str],
     export: Optional[str],
 ):
-    if not data_dir.is_dir():
-        raise FileNotFoundError(f'Dataset "{data_dir}" does not exists')
-
     return {
-        "data_dir": data_dir,
-        "cache": cache if cache else create_cache_path_from_data_dir(data_dir),
+        "dataset_json": dataset_json,
+        "cache": cache if cache else create_cache_path_from_dataset_json(dataset_json),
         "filter": parse_filter_string(filter) if filter else {},
         "output": json.loads(output) if output else {},
         "export": export,
     }
 
 
-def create_cache_path_from_data_dir(path: Path):
+def create_cache_path_from_dataset_json(path: Path):
     return Path("./hogehoge.sqlite3")
 
 
 def store_dataset(db: SqliteExtDatabase, dataset: Coco):
     # Create tables
     db.create_tables([Annotation, Category, Image], safe=True)
+
+    images = {image["id"]: image for image in dataset.images}
+    annotations = [
+        {
+            **annotation,
+            "area_ratio": (
+                annotation["area"]
+                / (images[annotation["image_id"]]["width"] * images[annotation["image_id"]]["height"])
+            ),
+        }
+        for annotation in dataset.annotations
+    ]
+
     # Save data
     with db.atomic():
         Category.insert_many(dataset.categories).execute()
         Image.insert_many(dataset.images).execute()
-        Annotation.insert_many(dataset.annotations).execute()
+        Annotation.insert_many(annotations).execute()
 
 
 def aggregate(annotations: Iterable[Annotation], filter: Dict):
@@ -89,7 +99,11 @@ def display_data(data: List[Dict], options: Dict):
     print(data)
 
 
-def export(data: List[Dict]):
+def export(
+    categories: List[Category],
+    images: List[Image],
+    annotations: List[Annotation],
+):
     # TODO: Export data
     pass
 
