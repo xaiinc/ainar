9ed75fe40b9e49ac549fa95e43295af9676eff3a
Masato Egashira <masato41280@icloud.com>
2022-02-09 15:33:21 +0900

    coco dataset (#21)
    * coco dataset
    
    * get info of image and annotations and classes

--- ainar/datasets/coco.py
+++ ainar/datasets/coco.py
@@ -1,5 +1,91 @@
 from pathlib import Path
-from typing import List, Tuple, TypedDict, Union
+from typing import List, Tuple, Union
+
+from pycocotools.coco import COCO
+from typing_extensions import TypedDict
+
+CLASSES = [
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "dining table",
+    "toilet",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
 
 
 class Image(TypedDict):
@@ -35,18 +121,32 @@ class Annotation(TypedDict):
 
 
 class Coco:
+    def __init__(
+        self,
+        annotation_file: Path,
+    ) -> None:
+
+        coco = COCO(str(annotation_file))
+        self.classes = CLASSES
+        self.annotations_list = list(coco.anns.values())
+        self.images_list = list(coco.imgs.values())
+        self.categories_list = list(coco.cats.values())
+
     @property
     def categories(self) -> List[Category]:
-        return []
+        return self.categories_list
 
     @property
     def images(self) -> List[Image]:
-        return []
+        return self.images_list
 
     @property
     def annotations(self) -> List[Annotation]:
-        return []
+        return self.annotations_list
+
+    def __len__(self) -> int:
+        return len(self.images_list)
 
 
 def load_dataset(path: Path):
-    return Coco()
+    return Coco(path)
